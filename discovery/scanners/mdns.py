8ebf7c633c8016e6d7171cdab8760ff73ae5c820
glop102 <alexlikeschess@gmail.com>
2026-03-29 02:21:13 -0400

    Remove the temporary re-exec test code in the mdns scanner

--- discovery/scanners/mdns.py
+++ discovery/scanners/mdns.py
@@ -4,10 +4,7 @@ from discovery.scanners.base_scanner import BaseScanner
 
 class MdnsScanner(BaseScanner):
     def start(self, args: list[str]):
-        print(args)
         _ = self.parse_args(args)
-        if os.getuid() != 0:
-            self.reexec()
         self.connect_to_server()
         # TODO - add an announce method to the base scanner that tells the server we are a scanner and gives the list of parameters that we take
         # TODO - check if the server connection closed and then exit
