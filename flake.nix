eb9a11c6be71c46a028cf0c97d5af34b63fffdd7
glop102 <alexlikeschess@gmail.com>
2026-04-07 02:16:07 -0400

    add pytest to the devshell

--- flake.nix
+++ flake.nix
@@ -57,6 +57,7 @@
                   scapy
                   tkinter
                   pydantic
+                  pytest
                 ]);
               shellHook = ''
                 export PS1='\n(dev) \[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] '
