4269b650e75519c5c4a1ad03a136dd3988794b69
glop102 <alexlikeschess@gmail.com>
2026-04-16 00:11:44 -0400

    Add tkinter as a dep for askpass

--- discovery/default.nix
+++ discovery/default.nix
@@ -1,9 +1,9 @@
-{ buildPythonPackage, setuptools, pydantic, scapy }:
+{ buildPythonPackage, setuptools, pydantic, scapy, tkinter }:
 buildPythonPackage {
   pname = "discovery";
   version = "0.1.0";
   pyproject = true;
   src = ./.;
   build-system = [ setuptools ];
-  dependencies = [ pydantic scapy ];
+  dependencies = [ pydantic scapy tkinter ];
 }
